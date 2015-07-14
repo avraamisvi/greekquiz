bbb4850e83b21d7b784875421abcad74169940cd
abraao.isvi <abraao.isvi@gmail.com>
2015-07-14 20:22:07 -0300

    creating quiz

--- www/js/quiz.js
+++ www/js/quiz.js
@@ -2,13 +2,41 @@ var quiz = {
 
   questions: [
     {
-      
+      title: "Qual é esta forma de λύω ?"
+      answers: [
+        {label:"a", text:"1a pessoa do presente do indicativo ativo singular"},
+        {label:"b", text:"1a pessoa do presente do indicativo ativo plural"},
+        {label:"c", text:"1a pessoa do futuro do indicativo ativo singular"},
+        {label:"d", text:"3a pessoa do futuro do indicativo passivo/médio singular"}
+      ],
+      correct: "a"
+    },
+    {
+      title: "Qual é esta forma de λύετε ?"
+      answers: [
+        {label:"a", text:"1a pessoa do presente do indicativo ativo singular"},
+        {label:"b", text:"2a pessoa do presente do indicativo ativo plural"},
+        {label:"c", text:"1a pessoa do futuro do indicativo ativo singular"},
+        {label:"d", text:"3a pessoa do futuro do indicativo passivo/médio singular"}
+      ],
+      correct: "b"
     }
   ],
+
+  actual: 0,
+  points: 0,
+  errors: 0,
+  max: 2,
 
   start: function (level, questions_len) {
 
   },
 
+  selectAnswer: function(label) {
 
+  },
+
+  next: function() {
+
+  }
 }
