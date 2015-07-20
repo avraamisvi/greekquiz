bc61e8d6fc250c45675002b167ef947ba367c9fe
abraao.isvi <abraao.isvi@gmail.com>
2015-07-20 19:43:25 -0300

    desenv

--- www/js/quiz.js
+++ www/js/quiz.js
@@ -16,32 +16,37 @@ var quiz = {
 
     this.loadQuestions(quiz_id);
 
-    $.get('quiz.mst', function(template) {
-      var rendered = Mustache.render(template, {config: {}});
-      $('#main_content').html(rendered);
-      quiz.loadQuestion();
-    });
-
   },
 
   loadQuestions: function(quiz_id) {
     //se as questos carregadas forem maior que o maximo, o maximo  permanece, senao se menor, muda para o total de questoes na lista.
-    $.get('data/'+quiz_id+'.js', function(data) {
+    $.get('data/'+quiz_id+'.dat', function(data) {
+      console.log(data)
       data = eval("(" + data + ")")
       quiz.data = data;
+
+      $.get('quiz.mst', function(template) {
+        var rendered = Mustache.render(template, {config: {}});
+        $('#main_content').html(rendered);
+        quiz.loadQuestion();
+      });
+
     });
 
   },
 
   rightAnswer: function() {
-    return this.getQuestion().answers[this.getQuestion().correct];
+    return this.data.answers[this.data.questions[this.atual].correct];
   },
 
   getQuestion: function() {
 
     var quest = {
         answers: [],
+        title:""
     };
+
+    console.log(this.data)
 
     var qtsAtual = this.data.questions[this.atual];
     var pattern = qtsAtual.patterns[Math.floor((Math.random() * 3))];
@@ -52,27 +57,28 @@ var quiz = {
         text: this.data.answers[pattern[i]].text,
         id: this.data.answers[pattern[i]].id
       });
+      quest.title = qtsAtual.title;
     }
 
     return quest;
   },
 
-  selectAnswer: function(label) {
-    this.selected = label;
+  selectAnswer: function(id) {
+    this.selected = id;
   },
 
   submitAnswer: function() {
 
-    if(this.selected == this.rightAnswer().label) {
+    if(this.selected == this.rightAnswer().id) {
       this.points++;
       $.get('result_succeed.mst', function(template) {
-        var rendered = Mustache.render(template, {right: quiz.rightAnswer()});
+        var rendered = Mustache.render(template, {right: quiz.rightAnswer(), explanation: quiz.data.questions[quiz.atual].explanation});
         $('#question_content').html(rendered);
       });
     } else {
       this.errors++;
       $.get('result_error.mst', function(template) {
-        var rendered = Mustache.render(template, {right: quiz.rightAnswer()});
+        var rendered = Mustache.render(template, {right: quiz.rightAnswer(), explanation: quiz.data.questions[quiz.atual].explanation});
         $('#question_content').html(rendered);
       });
     }
